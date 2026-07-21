0c84e40c642d4bf893a1833ae8fd2b6ed6dfa018
Maruf Hussen <marufhussen50414@gmail.com>
2026-07-21 18:58:12 +0200

    Hardcode Supabase URL and anon key
    Replaced environment variables with hardcoded Supabase URL and anon key.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = import.meta.env.VITE_SUPABASE_URL as string;
-const anon = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const url = "https://rdaergtnvfpjpgnkrodi.supabase.co";
+const anon = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkYWVyZ3RudmZwanBnbmtyb2RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNzQ5NTAsImV4cCI6MjA5OTk1MDk1MH0.7dhbli8An4DrgulH6EUQJHw7v2wgDZlxB0saggf0WoU";
 
 export const supabase = createClient(url, anon, {
   auth: { persistSession: true, autoRefreshToken: true },
