2dcd86a40ba335d8ae72bca4b361e1ccbe11675c
Maruf Hussen <marufhussen50414@gmail.com>
2026-07-29 23:45:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import AuthCallback from './components/AuthCallback'
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
